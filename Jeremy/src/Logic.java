518a33acc0d1494e3dee31f055f34abb49a3c6cc
Jeremy <jeremywhitenect@gmail.com>
2025-01-30 13:08:23 -0400

    Adds jerm section

---
+++ Jeremy/src/Logic.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Logic {
+    //private Board board;
+    private char currentPlayer;
+
+    public Logic() {
+        //board = new Board();
+        currentPlayer = 'X';
+    }
+
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
